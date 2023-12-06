ae7f4c19b33cafecf7faffc874a7bbfee5092c7b
Vassilis K <abaris.eiwar@mail.com>
2023-12-06 15:54:06 +0200

    Raw nmap command parsing & scanning

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 
 import scans.port_scanner
 
-def main():
 
-    scanResults = scans.port_scanner.performScan()
+def main():
+    # Nmap raw command; will be later taken from config, profile or input
+    nmapCommand = "nmap -Pn -T4 -p 22-443 127.0.0.1"
+
+    scanResults = scans.port_scanner.performScan(nmapCommand)
+
     print(scanResults)
+
 
 if __name__ == "__main__":
     main()
--- scans/port_scanner.py
+++ scans/port_scanner.py
@@ -1,18 +1,38 @@
 # TODO: Proper error handling
+# TODO: Validate host/hosts input
+# TODO: Further sanitize input
 
 import nmap
-# import logging
+import shlex
 
-def performScan():
+
+def performScan(rawCommand):
+    nmapArgs = shlex.split(rawCommand)
+
+    # Extract host
+    nmapHost = nmapArgs.pop(-1)
+
+    # Draft sanitization of raw command
+    if nmapArgs[0] == "nmap":
+        nmapArgs.pop(0)
+
     try:
-        scanner = nmap.PortScanner()
-        scanResults = scanner.scan('127.0.0.1', '22-443')
-        print(scanner.command_line())
+        # Check port argument
+        if not any(arg.startswith("-p") for arg in nmapArgs):
+            raise ValueError("No ports argument parsed.")
+
+        # Convert list to str for argument parsing
+        nmapArgs = " ".join(nmapArgs)
+
+        nm = nmap.PortScanner()
+
+        scanResults = nm.scan(nmapHost, arguments=nmapArgs)
+        scanResults = nm.csv()
 
     except Exception as e:
-        logf = open('error.log', 'a')
-        logf.write('nmap died: ' + str(e) + '\n')
-        print('New error log entry.')
-        scanResults = 'An error has occured.'
+        logf = open("error.log", "a")
+        logf.write("nmap died: " + str(e) + "\n")
+        print("New error log entry.")
+        scanResults = "An error has occurred."
 
     return scanResults
