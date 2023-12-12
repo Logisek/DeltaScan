681dfa10de93edd308b11acf00d30c9db015afb6
devm0nk <33984629+devm0nk@users.noreply.github.com>
2023-12-12 14:26:48 +0200

    Added simple UI, input validation, root check, error logging

--- main.py
+++ main.py
@@ -1,18 +1,88 @@
-# TODO: Check root permissions
-
 import scans.scanner
 import db.data_handler
+import inquirer
+import logging
+import os
+import re
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    # Nmap command, will later be taken from config, profile or input
-    host = "127.0.0.1"
-    arguments = "-sV -T4 --top-ports 3"
+    # checkRootPermissions()
+    action = getAction()
+    handleAction(action)
 
-    results = scans.scanner.scan(host, arguments)
+
+def view():
+    print("View")
+
+
+def checkRootPermissions():
+    if os.getuid() != 0:
+        logging.error("You need root permissions to run this program.")
+        print("You need root permissions to run this program.")
+        exit()
+
+
+def getAction():
+    options = [
+        inquirer.List(
+            "action",
+            message="What do you want to do?",
+            choices=["Scan", "View", "Exit"],
+        )
+    ]
+
+    return inquirer.prompt(options)
+
+
+def handleAction(action):
+    if action is not None:
+        if action["action"] == "Scan":
+            scan()
+        elif action["action"] == "View":
+            view()
+        elif action["action"] == "Exit":
+            exit()
+
+
+def validate_host(answers, current):
+    if not re.match(r"^[a-zA-Z0-9.-/]+$", current):
+        return "Invalid host. Please enter a valid IP address, domain name, or network address."
+    return True
+
+
+def validate_arguments(answers, current):
+    if not re.match(r"^[a-zA-Z0-9\s-]+$", current):
+        return "Invalid arguments. Please enter only alphanumeric characters, spaces, and hyphens."
+    return True
+
+
+def scan():
+    # Validate user input
+    options = [
+        inquirer.Text(
+            "host", message="Enter target host or network", validate=validate_host
+        ),
+        inquirer.Text(
+            "arguments", message="Nmap arguments", validate=validate_arguments
+        ),
+    ]
+
+    answers = inquirer.prompt(options)
+
+    results = None
+    arguments = None
+
+    if answers is not None:
+        host = answers["host"]
+        arguments = answers["arguments"]
+
+        results = scans.scanner.scan(host, arguments)
 
     if results is None:
-        print("An error as occurred")
+        logging.error("An error occurred during the scan. Please check your host and arguments.")
         return
 
     save = db.data_handler.DataHandler()
